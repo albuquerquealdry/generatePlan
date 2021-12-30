3923c009dc5ff31e04cdb972289a1e8d388e508f
albuquerquealdry <albuquerquealdry@gmail.com>
2021-12-30 19:23:20 -0300

    commit

--- src/generate/service/generate.service.ts
+++ src/generate/service/generate.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GenerateService {
     create(body){
-        console.log('teste')
-        return body 
+      const object ={
+        body,
+        "name": "aldry"
+      }
+      return body 
       }
 }
